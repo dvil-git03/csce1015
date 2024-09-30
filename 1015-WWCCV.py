dcfda955db55398ed6286b88e4eb6ea07059cbdd
Diego Villela <diegovillelamorua@my.unt.edu>
2024-09-30 10:26:33 -0500

    Diego implemented luhn and testing

--- 1015-WWCCV.py
+++ 1015-WWCCV.py
@@ -1,7 +1,11 @@
+import luhn
 # Credit Card Validator version .1
 # This is the world's worst credit card number validator. It checks to see if the number is 16 digits long and that is it.  
 
 # anything that starts with a pound sign (hashtag if you are under 30) is a comment. The computer ignores these.
+
+def is_credit_card_valid(card_number):
+    return luhn.verify(card_number)
 
 # Let's tell the world whose awful credit card validator this is.
 print ("Diego Villela Modified This Credit Card Validator")
@@ -17,4 +21,18 @@ card_number = input("Enter your 16-digit credit card number: ")
 if len(card_number) == 16 and card_number.isdigit():     
     print ("Card is valid.")
 else:
-    print ("Invalid card number. It must be 16 digits long.") 
+    print ("Invalid card number. It must be 16 digits long.")
+
+def run_tests():
+
+    assert is_credit_card_valid("4111111111111111"), '4111111111111111 should pass but did not'
+
+    assert is_credit_card_valid("5105105105105100"), '5105105105105100 should pass but did not'
+
+    assert not is_credit_card_valid("134"), '134 should not pass but did'
+
+    assert not is_credit_card_valid("1234567890123456"), '1234567890123456 should not pass but did'
+
+    assert not is_credit_card_valid("000000000000"), 'This is a bad test and we will get an error message'
+
+run_tests() 
